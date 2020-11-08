c5f623e7d3f2eb47b0b552dc4699956b3b396134
ChristianCaldwell55 <christian.c2969@gmail.com>
2020-11-08 14:19:31 -0600

    Add additional functionality, such as making a circle and finding mission pads

--- Code/telloFlight.py
+++ Code/telloFlight.py
@@ -1,29 +1,29 @@
 # import easyTello API
 from easyTello.easytello import tello
+# import custom action functions
+from telloFunctions import *
 
 # initialize drone object
-drone = tello.Tello()
+# drone = tello.Tello()
 # while True, keep accepting user input for Tello commands
 drone_in_flight = True
 
 # UI: Allow user to see custom command and other selections
-print('Tello User Interface\nEnter Tello commands listed in SDK 2.0\nAdditional Functions:\n1. Exit\n2. square-dance')
-
-def squareDance():
-    drone.takeoff()
-    for i in range(4):
-        drone.forward(100)
-        drone.flip('f')
-        drone.cw(90)
-    drone.land()
+print('Tello User Interface\nEnter Tello commands listed in SDK 2.0\nAdditional Functions:\n1. Exit\n2. square-dance\n3. go-to-m1')
 
 while(drone_in_flight):
     user_command = input('Enter a command: ')
     # end condition
     if(user_command == 'exit'):
         drone.land()
+        drone.close()
         drone_in_flight = False
     elif(user_command == 'square-dance'):
         squareDance()
+    elif(user_command == 'go-to-m1'):
+        goToM1()
+    elif(user_command == 'make-circle'):
+        makeCircle()
+    # SDK command
     else:
         drone.send_command(user_command, True)
---
+++ Code/telloFunctions.py
@@ -0,0 +1,69 @@
+# import easyTello API
+from easyTello.easytello import tello
+
+# initialize drone object
+drone = tello.Tello()
+
+def squareDance():
+    drone.takeoff()
+    for i in range(4):
+        drone.forward(100)
+        drone.flip('f')
+        drone.cw(90)
+    drone.land()
+
+def makeCircle():
+    drone.takeoff()
+    drone.curve(70,70,0,140,0,0,60)
+    drone.cw(180)
+    drone.curve(70,70,0,140,0,0,60)
+    drone.land()
+
+# misson pad functions
+def goToM1():
+    drone.takeoff()
+    drone.mon()
+    drone.go(100, 100, 100, 60, 'm1')
+    drone.land()
+
+def goToM2():
+    drone.takeoff()
+    drone.mon()
+    drone.go(100, 100, 100, 60, 'm2')
+    drone.land()
+
+def goToM3():
+    drone.takeoff()
+    drone.mon()
+    drone.go(100, 100, 100, 60, 'm3')
+    drone.land()
+
+def goToM4():
+    drone.takeoff()
+    drone.mon()
+    drone.go(100, 100, 100, 60, 'm4')
+    drone.land()
+
+def goToM5():
+    drone.takeoff()
+    drone.mon()
+    drone.go(100, 100, 100, 60, 'm5')
+    drone.land()
+
+def goToM6():
+    drone.takeoff()
+    drone.mon()
+    drone.go(100, 100, 100, 60, 'm6')
+    drone.land()
+
+def goToM7():
+    drone.takeoff()
+    drone.mon()
+    drone.go(100, 100, 100, 60, 'm7')
+    drone.land()
+
+def goToM8():
+    drone.takeoff()
+    drone.mon()
+    drone.go(100, 100, 100, 60, 'm8')
+    drone.land()
